7560455eef7f54486138026b87e9cc53fab26b75
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-10 15:47:26 +0200

    tp7 ou 3 de demain

--- src/main/java/fr/diginamic/banque/TestBanque.java
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -1,12 +1,23 @@
 package fr.diginamic.banque;
 
+import java.util.ArrayList;
+
 import fr.diginamic.banque.entites.Compte;
+import fr.diginamic.banque.entites.CompteTaux;
 
 public class TestBanque {
 
 	public static void main(String[] args) {
 		Compte c1 = new Compte(1, 7.5);
-		System.out.println(c1);
+		CompteTaux ct1 = new CompteTaux(1, 7.5, 8);
+		//System.out.println(ct1);
 		
+		ArrayList<Compte> tab = new ArrayList<Compte>();
+		tab.add(c1);
+		tab.add(ct1);
+		
+		for(int i = 0; i < tab.size(); i++) {
+			System.out.println(tab.get(i));
+		}
 	}
 }
---
+++ src/main/java/fr/diginamic/banque/entites/CompteTaux.java
@@ -0,0 +1,23 @@
+package fr.diginamic.banque.entites;
+
+public class CompteTaux extends Compte {
+	private double tauxRemun;
+
+	public CompteTaux(int numero, double solde, double tauxRemun) {
+		super(numero, solde);
+		this.tauxRemun = tauxRemun;
+	}
+
+	public double getTauxRemun() {
+		return tauxRemun;
+	}
+
+	public void setTauxRemun(double tauxRemun) {
+		this.tauxRemun = tauxRemun;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString().substring(0, super.toString().length() - 1) +  ", tauxRemun=" + tauxRemun + "]";
+	}
+}
