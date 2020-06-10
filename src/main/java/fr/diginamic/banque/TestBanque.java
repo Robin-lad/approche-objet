4fd71fd8c8a28147072f94108a42e5e9188719b8
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-10 15:21:39 +0200

    TP5 ou tp1 du j2

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,12 @@
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte c1 = new Compte(1, 7.5);
+		System.out.println(c1);
+		
+	}
+}
