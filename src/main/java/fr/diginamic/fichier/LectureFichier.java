3f4dcb6e4a8598dc27fe5e3849f17964d3d6d2f8
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-17 11:00:43 +0200

    tp6 j6

---
+++ src/main/java/fr/diginamic/fichier/LectureFichier.java
@@ -0,0 +1,37 @@
+/** 
+*
+*/
+package fr.diginamic.fichier;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * @author robin
+ *
+ */
+public class LectureFichier {
+
+	/**
+	 * @param args void
+	 */
+	public static void main(String[] args) {
+
+		try {
+			File f = new File("C:/temp/recensement_population_2016.csv");
+			List<String> lignes = FileUtils.readLines(f, "UTF-8");
+
+			for (String ligne : lignes) {
+				System.out.println(ligne);
+			}
+
+			System.out.println("Nombre de lignes :" + lignes.size());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
